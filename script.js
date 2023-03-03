015ec9b2e9ddcaf4682e2de0bf5ee7df34e9ba50
Priyanka Pol <116531017+Priyanka-Pol@users.noreply.github.com>
2023-03-03 22:01:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 let yourArray = [{name:"Priya",age:21,isLoggedIn:true}, ['apple', 42], true, 23, 'hello world'];
-// console.log(yourArray.length);
-console.log(yourArray);
+console.log(yourArray.length);
+// console.log(yourArray);
