546d83a23dd71152a0e25883a6245624f9993e2a
Priyanka Pol <116531017+Priyanka-Pol@users.noreply.github.com>
2023-03-03 22:06:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ for(let i=0;i<5;i++){
 console.log(yourArray.length);
 
 
-let yourArray = [{name:"Priya",age:21,isLoggedIn:true}, ['apple', 42], true, 23, 'hello world'];
-console.log(yourArray.length);
-// console.log(yourArray);
+// let yourArray = [{name:"Priya",age:21,isLoggedIn:true}, ['apple', 42], true, 23, 'hello world'];
+// console.log(yourArray.length);
+// // console.log(yourArray);
